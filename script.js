33705a91d85fef7084f3d284f2dbfbb5af232b73
Glitch (glitch-hello-website) <none>
2022-06-13 15:45:07 +0000

    🏢💽 Checkpoint ./index.html:22221649/267 ./script.js:22221649/319

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    showText : true,
+    items:
+    {text:'item a',color:'blue',text:'item b',color:'green'}
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
   }
 })
 function toggle(con){
